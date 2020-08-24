dd179336962a2fc83dcfca314bf283e80ff65374
Aadil Hussain <aadil.hussain@duke.edu>
2020-08-24 13:13:44 -0400

    Added HDL driver interpretation to input and analyze HDL levels

--- calculator.py
+++ calculator.py
@@ -9,13 +9,27 @@ def interface():
             return
         elif choice =='1':
             HDL_driver()
+            return
 def HDL_driver():
     # Get input
     HDL_result = get_input()
     #Check if HDL is normal
+    Interpretation = check_HDL(HDL_result)
     #Output
+    output_HDL(HDL_result, Interpretation)
+
 def get_input():
     HDL_input = input("Enter your HDL")
     return int(HDL_input)
-def Check
+
+def check_HDL(HDL_result):
+    if HDL_result >= 60:
+        return "Normal"
+    elif 40 <= HDL_result <60:
+        return "Borderline Low"
+    else:
+        return "Low"
+def output_HDL(HDL_result, Interpretation):
+    print("The HDL result is {}".format(HDL_result))
+    print("That is {}".format(Interpretation))
 interface()
