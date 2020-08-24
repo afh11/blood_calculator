f6c6c8e16357255d581532dd38fe47e530543c47
Aadil Hussain <aadil.hussain@duke.edu>
2020-08-24 12:55:37 -0400

    fixed option prompt line

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 def interface():
     print("My Program")
     while True: #Simple way of creating an endless loop
-        print("Options:")
+        print("Options for you")
         print("9 - Quit")
         choice = input("Enter your choice: ")
         if choice=='9':
