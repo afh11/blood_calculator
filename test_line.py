ecd70a0add586266de5a957cce636f75106f2441
Aadil Hussain <aadil.hussain@duke.edu>
2020-09-02 13:44:38 -0400

    fixed using wrong variable to check result against

--- test_line.py
+++ test_line.py
@@ -1,10 +1,9 @@
 import pytest
 #function that receipes two tuples and an x value and returns the y value.
-@pytest.mark.parametrize("x1, y1, x2, y2, x3, x4", [
+@pytest.mark.parametrize("x1, y1, x2, y2, x3, y4", [
 	(1, 10, 4, 25, 6, 35),
 ])
-def test_return_y(x1, y1, x2, y2, x3, x4):
+def test_return_y(x1, y1, x2, y2, x3, y4):
 	from line import return_y
 	result = return_y(x1, y1, x2, y2, x3)
-	expected == x4
-	assert result == expected
+	assert result == y4
